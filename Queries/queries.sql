5ed74e3b5d69222642456a0c2fa426a676590c21
Amanda Thomson <acfthomson@gmail.com>
2021-01-20 13:12:16 -0500

    some Deliverable 1 progress

--- Queries/queries.sql
+++ Queries/queries.sql
@@ -1,24 +1,3 @@
--- Retirement eligibility
-SELECT first_name, last_name
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
---Number of employees retiring
-SELECT COUNT(first_name)
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
--- Create a retirement table
-SELECT first_name, last_name
-INTO retirement_info
-FROM employees
-WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
-AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
-SELECT * FROM retirement_info
-
 -- Joining departments and dept_manager tables
 SELECT d.dept_name,
 	dm.emp_no,
