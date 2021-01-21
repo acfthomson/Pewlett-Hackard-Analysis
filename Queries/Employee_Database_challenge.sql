304dbfef49e83fcff6d9798ce24eeb44ce9f005a
Amanda Thomson <acfthomson@gmail.com>
2021-01-21 15:27:24 -0500

    Deliverable 2 progress

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -12,7 +12,6 @@ ON (e.emp_no = t.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no, title;
 
-
 -- Use Dictinct with Orderby to remove duplicate rows to get most recent titles
 SELECT DISTINCT ON (emp_no) emp_no,
 first_name,
@@ -22,10 +21,27 @@ INTO unique_titles
 FROM retirement_titles
 ORDER BY emp_no, to_date DESC;
 
-
 -- Retrieve number of employees by most recent job title about to retire
 SELECT count(title), title
 INTO retiring_titles
 FROM unique_titles
 GROUP BY title
 ORDER BY COUNT DESC;
+
+-- Create a mentorship eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+       e.first_name,
+	   e.last_name,
+	   e.birth_date,
+	   de.from_date,
+	   de.to_date,
+	   t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
